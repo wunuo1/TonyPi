ea2d9e35a5f117cbcfc96e3bf05a87b5eea119df
root <wunuo>
2024-09-03 10:43:03 +0800

    Init X5

--- test.py
+++ test.py
@@ -6,8 +6,14 @@ import hiwonder.ActionGroupControl as AGC
 
 AGC.runActionGroup('stand')
 AGC.runActionGroup('right_move')
-Board.setPWMServoPulse(1, 1100, 100)
-Board.setPWMServoPulse(2, 1100, 100)
-Board.setBusServoPulse(3, 500, 200)
+Board.setPWMServoPulse(1, 2000, 500)
+time.sleep(0.5)
+Board.setPWMServoPulse(1, 1500, 0)
+time.sleep(1)
+Board.setPWMServoPulse(2, 2000, 1000)
+time.sleep(1)
 
+
+if __name__ == '__main__':
+    main()
 
